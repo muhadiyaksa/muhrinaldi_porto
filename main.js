c43d0ecad8b1625caab88db7e263c915fa110235
muhadiyaksa <muhadiyaksa@gmail.com>
2022-08-14 20:22:20 +0700

    Update Final

---
+++ main.js
@@ -0,0 +1,14 @@
+const emailTujuan = document.querySelector("#email");
+const subjekTujuan = document.querySelector("#subject");
+const bodyTujuan = document.querySelector("#body");
+
+const tombolKirim = document.querySelector("#kirim");
+tombolKirim.addEventListener("click", function () {
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (emailTujuan.value !== "" && emailTujuan.value.match(validRegex) && subjekTujuan.value !== "" && bodyTujuan.value !== "") {
+    tombolKirim.setAttribute("href", `mailto:${emailTujuan.value}?subject=${subjekTujuan.value}&body=${bodyTujuan.value}`);
+  } else {
+    alert("Value Is not valid");
+  }
+});
